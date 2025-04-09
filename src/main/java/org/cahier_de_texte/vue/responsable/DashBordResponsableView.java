9d6898d307c980617d2860e59a8906eaebeffa94
diass <iboudiassy78@gmail.com>
2025-04-09 22:01:44 +0000

    Tableau de bord responsable

--- src/main/java/org/cahier_de_texte/vue/responsable/DashBordResponsableView.java
+++ src/main/java/org/cahier_de_texte/vue/responsable/DashBordResponsableView.java
@@ -3,5 +3,7 @@ package org.cahier_de_texte.vue.responsable;
 import javax.swing.*;
 
 public class DashBordResponsableView extends JFrame {
-
+    public DashBordResponsableView(){
+        System.out.println("Hello world");
+    }
 }
